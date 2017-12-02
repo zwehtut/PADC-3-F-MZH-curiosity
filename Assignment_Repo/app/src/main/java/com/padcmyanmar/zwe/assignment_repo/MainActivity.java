5f7c794c73e1f296f8722eacc393a88e734f60d9
hp\ZWE <mtkmzh@gmail.com>
2017-12-02 11:39:18 +0630

    Curiosity Screen -Layout xml

---
+++ Assignment_Repo/app/src/main/java/com/padcmyanmar/zwe/assignment_repo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.padcmyanmar.zwe.assignment_repo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
